34b2b739bdd99f8e5c45e56f9e31c5947fad1ca2
Namila Perera <namila@ubuntu.(none)>
2015-06-08 03:06:06 -0700

    Basic Programme Added

--- mpich-rsaDecrypt.c
+++ mpich-rsaDecrypt.c
@@ -5,6 +5,8 @@ int totalProcesses;
 int currentProcessRank;
 int main(int argc,char **argv){
 	MPI_Init(&argc,&argv);
-	MPI_Comm_size(MPI_COMM_WORLD);
+	MPI_Comm_size(MPI_COMM_WORLD,&totalProcesses);
+	MPI_Comm_rank(MPI_COMM_WORLD,&currentProcessRank);
+	printf("Total Processes %d Current Rank %d\n",totalProcesses,currentProcessRank);
 	return 0;
 }
