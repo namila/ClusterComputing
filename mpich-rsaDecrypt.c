0c105df1ce824150ea84e2d64cb0a00132d13892
Namila Perera <namila@ubuntu.(none)>
2015-06-09 10:49:36 -0700

    prime number generation added

--- mpich-rsaDecrypt.c
+++ mpich-rsaDecrypt.c
@@ -1,12 +1,22 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <list>
 
 // Process 0 will act as the root process
 #define rootProcess 0
 
+using namespace std;
+
 int totalProcesses;
 int currentProcessRank;
 int codeToBeCracked;
+int flag;
+int inputsForOneProcess;
+list<int> primeNumbersList;
+list<int> fullprimeNumberList;
+
+int GetPrimesInGivenRange(int lowerBound,int upperBound);
+
 
 
 int main(int argc,char **argv){
@@ -21,16 +31,62 @@ int main(int argc,char **argv){
 	MPI_Comm_rank(MPI_COMM_WORLD,&currentProcessRank);
 
 	if(currentProcessRank==rootProcess){
+
 		// Getting the RSA code to be cracked
 		printf("Enter tha RSA Code to be Cracked\n");
 		scanf("%d",&codeToBeCracked);
+		inputsForOneProcess=codeToBeCracked/totalProcesses;
 		
-		// 
+		// Value of The RSA code is less than the number of processors
+		if(inputsForOneProcess==0){
+			GetPrimesInGivenRange(0,codeToBeCracked);	
+		}
+
+		else{
+			GetPrimesInGivenRange(inputsForOneProcess*currentProcessRank+1,inputsForOneProcess*(currentProcessRank+1));
+		}
+
+		
+
+		
+		
+		
 		
 	}
 
 
 
 	//printf("Total Processes %d Current Rank %d\n",totalProcesses,currentProcessRank);
+	return 0;
+}
+
+
+//Generating the prime Numbers in the given Range
+
+int GetPrimesInGivenRange(int lowerBound,int upperBound)
+{
+
+	for(int i=lowerBound; i<=upperBound; ++i)
+	{
+
+		flag=0;
+
+		for(int j=2; j<=i/2; ++j)
+		{
+         		if(i%j==0)
+         		{
+            			flag=1;
+            			break;
+         		}
+      		}
+
+		if(flag==0)
+		{
+			primeNumbersList.push_back(i);
+			printf("%d\n",i);
+		}
+         
+  	}
+
 	return 0;
 }
