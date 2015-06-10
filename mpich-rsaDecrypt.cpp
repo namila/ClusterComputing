77cfbca1f39f01cb8207491f40b15d5b25fae931
Namila Perera <namila@ubuntu.(none)>
2015-06-10 13:12:41 -0700

    prime number generation done

--- mpich-rsaDecrypt.cpp
+++ mpich-rsaDecrypt.cpp
@@ -1,12 +1,12 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <math.h>
 #include <list>
+//#include <iostream>
 
 // Process 0 will act as the root process
 #define rootProcess 0
-
 using namespace std;
-
 int totalProcesses;
 int currentProcessRank;
 int codeToBeCracked;
@@ -20,6 +20,7 @@ int GetPrimesInGivenRange(int lowerBound,int upperBound);
 
 
 int main(int argc,char **argv){
+	
 
 	// Initializing the MPI Environment
 	MPI_Init(&argc,&argv);
@@ -30,31 +31,37 @@ int main(int argc,char **argv){
 	// Determining the rank of current proces
 	MPI_Comm_rank(MPI_COMM_WORLD,&currentProcessRank);
 
+	
 	if(currentProcessRank==rootProcess){
 
 		// Getting the RSA code to be cracked
 		printf("Enter tha RSA Code to be Cracked\n");
-		scanf("%d",&codeToBeCracked);
-		inputsForOneProcess=codeToBeCracked/totalProcesses;
-		
-		// Value of The RSA code is less than the number of processors
-		if(inputsForOneProcess==0){
-			GetPrimesInGivenRange(0,codeToBeCracked);	
-		}
-
-		else{
-			GetPrimesInGivenRange(inputsForOneProcess*currentProcessRank+1,inputsForOneProcess*(currentProcessRank+1));
-		}
-
-		
-
-		
-		
-		
-		
+		scanf("%d",&codeToBeCracked);						
 	}
 
+	// Synchronizing the processes before the broadcast
+	MPI_Barrier(MPI_COMM_WORLD);
 
+	// Broadcasting user inputs to other processes
+	MPI_Bcast(&codeToBeCracked,1,MPI_INT,rootProcess,MPI_COMM_WORLD);
+
+	// Synchronizing after the broadcast
+	MPI_Barrier(MPI_COMM_WORLD); 
+
+	//printf("processRank %d inputData %d\n",currentProcessRank,codeToBeCracked);
+
+	inputsForOneProcess=codeToBeCracked/totalProcesses;
+		
+	// Value of The RSA code is less than the number of processors
+	if(inputsForOneProcess==0){
+		GetPrimesInGivenRange(0,codeToBeCracked);	
+	}
+
+	else{
+		GetPrimesInGivenRange(inputsForOneProcess*currentProcessRank+1,inputsForOneProcess*(currentProcessRank+1));
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
 
 	//printf("Total Processes %d Current Rank %d\n",totalProcesses,currentProcessRank);
 	return 0;
@@ -83,7 +90,7 @@ int GetPrimesInGivenRange(int lowerBound,int upperBound)
 		if(flag==0)
 		{
 			primeNumbersList.push_back(i);
-			printf("%d\n",i);
+			printf("processId%d   %d\n",currentProcessRank,i);
 		}
          
   	}
