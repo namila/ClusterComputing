cb9d4a13b9afe7db60e67667398791c11db8140f
Namila Perera <namila@ubuntu.(none)>
2015-06-11 12:07:54 -0700

    Item aggregation attempted but not 100 precent sucessful

--- mpich-rsaDecrypt.cpp
+++ mpich-rsaDecrypt.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <list>
+#include <stdlib.h>
 //#include <iostream>
 
 // Process 0 will act as the root process
@@ -12,8 +13,19 @@ int currentProcessRank;
 int codeToBeCracked;
 int flag;
 int inputsForOneProcess;
-list<int> primeNumbersList;
+int tag=100;
+int numberOfElementsFiled;
+list<int> primeNumbersListForAProcess;
 list<int> fullprimeNumberList;
+int *primeNumberArrayForAProcess;
+int *primeNumbersFromAllProcesses;
+int *primeValuesDistribution;
+int *resultArray;
+int *offSets;
+
+
+MPI_Datatype variableType;
+MPI_Status messageStatus;
 
 int GetPrimesInGivenRange(int lowerBound,int upperBound);
 
@@ -37,9 +49,15 @@ int main(int argc,char **argv){
 		// Getting the RSA code to be cracked
 		printf("Enter tha RSA Code to be Cracked\n");
 		scanf("%d",&codeToBeCracked);	
-		printf("Prime numbers Between 0 and %d\n",codeToBeCracked);					
+		//primeNumbersFromAllProcesses=(int *)malloc(sizeof(int)*codeToBeCracked);
+
+		printf("Prime numbers Between 0 and %d\n",codeToBeCracked);			
+					
 	}
 
+	primeValuesDistribution=(int *)malloc(sizeof(int)*totalProcesses);
+	resultArray=(int *)malloc(sizeof(int)*1);
+	offSets=(int *)malloc(sizeof(int)*totalProcesses);
 	// Synchronizing the processes before the broadcast
 	MPI_Barrier(MPI_COMM_WORLD);
 
@@ -71,6 +89,52 @@ int main(int argc,char **argv){
 
 	MPI_Barrier(MPI_COMM_WORLD);
 
+		
+	// Getting the number of primes found by the process and putting them into an array
+	
+	resultArray[0]=primeNumbersListForAProcess.size();
+	numberOfElementsFiled=resultArray[0];
+	printf("wow p:%d %d\n",currentProcessRank,numberOfElementsFiled);
+	
+	MPI_Gather(resultArray,1,MPI_INT,primeValuesDistribution,1,MPI_INT,rootProcess,MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if(currentProcessRank==rootProcess){
+		primeNumbersFromAllProcesses=(int *)malloc(sizeof(int)*codeToBeCracked);
+		offSets[0]=0;
+		for(int i=1;i<totalProcesses;++i){
+			offSets[i]=offSets[i-1]+primeValuesDistribution[i-1];
+			
+		}
+
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	primeNumberArrayForAProcess=(int *)malloc(sizeof(int)*numberOfElementsFiled);
+	list<int>::iterator primeNumberIterator = primeNumbersListForAProcess.begin();
+
+	for(int i=0;i<numberOfElementsFiled;++i){
+		primeNumberArrayForAProcess[i]=	*primeNumberIterator;
+		//printf("Validator %d\n",primeNumberArrayForAProcess[i]);
+		++primeNumberIterator;
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	 
+	MPI_Gatherv(primeNumberArrayForAProcess,primeValuesDistribution[currentProcessRank],MPI_INT,primeNumbersFromAllProcesses,primeValuesDistribution,offSets,MPI_INT,rootProcess,MPI_COMM_WORLD);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	
+
+	if(currentProcessRank==rootProcess){
+		for(int i=0;i<codeToBeCracked;++i){
+			printf("Final %d : %d\n",i,primeNumbersFromAllProcesses[i]);
+		}
+		
+	}
+	
+	MPI_Barrier(MPI_COMM_WORLD);
 	
 	return 0;
 }
@@ -95,13 +159,13 @@ int GetPrimesInGivenRange(int lowerBound,int upperBound)
          		}
       		}
 
-		if(flag==0)
+		if(flag==0 & (i!=1)& (i!=2))
 		{
-			primeNumbersList.push_back(i);
+			primeNumbersListForAProcess.push_back(i);
 			printf("Counteed by Process %d   %d\n",currentProcessRank,i);
 		}
          
   	}
-
+	
 	return 0;
 }
